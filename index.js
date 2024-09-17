452e8d583f73c86ee6024f307ad6633a1a80daf2
Rafa <rafafajari230907@gmail.com>
2024-09-17 16:23:55 +0700

    update index.js

--- index.js
+++ index.js
@@ -8,8 +8,5 @@ app.use(express.json());
 // Gunakan rute API
 app.use('/api', apiRoutes);
 
-// Jalankan server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server berjalan di http://localhost:${PORT}`);
-});
+// Jangan gunakan app.listen di Vercel
+module.exports = app;
