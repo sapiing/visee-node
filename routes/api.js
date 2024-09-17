ac0cff04e1552f4f51378d07eca37c5d5cbc75d8
Rafa <rafafajari230907@gmail.com>
2024-09-17 18:12:06 +0700

    add dwellTimeZone and Period API

--- routes/api.js
+++ routes/api.js
@@ -7,13 +7,15 @@ const gender_ratio = require('../data/dummyGenderRatio.json');
 const heatmap = require('../data/dummyHeatmap.json');
 const peakTime = require('../data/dummyPeakTime.json');
 const peakDay = require('../data/dummyPeakDay.json');
+const dwellTimeZone = require('../data/dummyDwellTimeZone.json');
+const dwellTimePeriod = require('../data/dummyDwellTimePeriod.json');
 
 // Define rute untuk masing-masing data
 router.get('/count', (req, res) => {
   res.json(count);
 });
 
-router.get('/gender_ratio', (req, res) => {
+router.get('/gender-ratio', (req, res) => {
   res.json(gender_ratio);
 });
 
@@ -21,12 +23,20 @@ router.get('/heatmap', (req, res) => {
   res.json(heatmap);
 });
 
-router.get('/peak_time', (req, res) => {
+router.get('/peak-time', (req, res) => {
   res.json(peakTime);
 });
 
-router.get('/peak_day', (req, res) => {
+router.get('/peak-day', (req, res) => {
   res.json(peakDay);
+});
+
+router.get('/dwell-time-zone', (req, res) => {
+  res.json(dwellTimeZone);
+});
+
+router.get('/dwell-time-period', (req, res) => {
+  res.json(dwellTimePeriod);
 });
 
 module.exports = router;
