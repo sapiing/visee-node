2b8e6edab2eb4ed76247682885b3a0ad4ca29bd9
Rafa <rafafajari230907@gmail.com>
2024-09-18 13:59:13 +0700

    add CORS handling

--- app.js
+++ app.js
@@ -8,8 +8,15 @@ app.use(express.json());
 // Gunakan rute API
 app.use('/api', apiRoutes);
 
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Sesuaikan dengan metode yang Anda gunakan
+    allowedHeaders: ['Content-Type', 'Authorization'] // Sesuaikan jika perlu
+}));
+
 // Jalankan server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server berjalan di http://localhost:${PORT}`);
 });
+
