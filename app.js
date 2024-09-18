a03d2da97b1e50108fe527bf1f63ac32d39f3961
Rafa <rafafajari230907@gmail.com>
2024-09-18 14:04:02 +0700

    add CORS handling (pls be working)

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ const apiRoutes = require('./routes/api');
 // Middleware untuk parsing JSON
 app.use(express.json());
 
-// Gunakan rute API
-app.use('/api', apiRoutes);
-
 app.use(cors({
     origin: 'http://localhost:3000',
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Sesuaikan dengan metode yang Anda gunakan
     allowedHeaders: ['Content-Type', 'Authorization'] // Sesuaikan jika perlu
 }));
+
+// Gunakan rute API
+app.use('/api', apiRoutes);
 
 // Jalankan server
 const PORT = process.env.PORT || 5000;
